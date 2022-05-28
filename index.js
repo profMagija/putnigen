fdfef9d79ebd67e89ad01e5674627e3dd7c314ef
Nikola Bebić <nikola.bebic99@gmail.com>
2022-05-28 08:54:20 +0200

    export & rukovodioc

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const R_FIELDS = (
     'imeprezime status jmbg stalnaadresa privadresa brlk mup telefon dolazak odlazak ' +
     'od5 do5 jvp5 cp5 od6 do6 jvp6 cp6 od7 do7 jvp7 cp7 od8 do8 jvp8 cp8 ' +
-    'od1 do1 vp1 od2 do2 vp2 od3 do3 vp3 od4 do4 vp4 ' + 
-    'razdaljina cenag vrstag regbr ' + 
+    'od1 do1 vp1 od2 do2 vp2 od3 do3 vp3 od4 do4 vp4 ' +
+    'razdaljina cenag vrstag regbr ' +
     'ostalo1 costalo1 ostalo2 costalo2 ostalo3 costalo3 ostalo4 costalo4'
 ).split(' ');
 
@@ -11,17 +11,18 @@ const G_FIELDS = 'program sifrap datump datumk poziv'.split(' ');
 const DATE_FIELDS = 'datump datumk dolazak odlazak'.split(' ');
 
 const SPV_FIELDS = (
-    'od1 do1 vp1 od2 do2 vp2 od3 do3 vp3 od4 do4 vp4 ' + 
+    'od1 do1 vp1 od2 do2 vp2 od3 do3 vp3 od4 do4 vp4 ' +
     'razdaljina cenag vrstag regbr'
 ).split(' ');
 
 const JPV_FIELDS = 'od5 do5 jvp5 cp5 od6 do6 jvp6 cp6 od7 do7 jvp7 cp7 od8 do8 jvp8 cp8'.split(' ');
 
 const DNEVNICA_MAP = {
-    'Saradnik': 850*2,
+    'Saradnik': 850 * 2,
     'Stručni': 850,
-    'Mentor': 850*2,
+    'Mentor': 850 * 2,
     'Asistent': 850,
+    'Rukovodioc': 850 * 5,
 }
 
 const single_row = d => `
@@ -46,6 +47,7 @@ const single_row = d => `
         <option value="Stručni">Stručni konsultant</option>
         <option value="Mentor">Mentor</option>
         <option value="Asistent">Asistent</option>
+        <option value="Rukovodioc">Rukovodioc</option>
     </select>
 </td>
 <td>
@@ -142,11 +144,11 @@ function new_saradnik() {
     const s = {
         id: "" + (+new Date()),
     };
-    
+
     R_FIELDS.forEach(rf => {
         s[rf] = "";
     });
-    
+
     return s;
 }
 
@@ -167,13 +169,13 @@ function update() {
     G_FIELDS.forEach(gf => {
         DATA[gf] = document.getElementById(`g-${gf}`).value;
     });
-    
+
     DATA.saradnici.forEach(sd => {
         R_FIELDS.forEach(rf => {
             sd[rf] = document.getElementById(`r-${sd.id}-${rf}`).value;
         })
     });
-    
+
     render();
 }
 
@@ -190,12 +192,12 @@ function reset_seminar() {
     DATA.datump = "";
     DATA.datumk = "";
     DATA.sifrap = "";
-    
+
     DATA.saradnici.forEach(sr => {
         sr.dolazak = "";
         sr.odlazak = "";
     });
-    
+
     render();
 }
 
@@ -217,21 +219,21 @@ function get_saradnik_by_id(s_id) {
 
 function stampaj_saradnik(s_id) {
     let s_data = get_saradnik_by_id(s_id);
-    
+
     let formdata = {};
-    
+
     G_FIELDS.forEach(gf => {
         if (!DATE_FIELDS.includes(gf)) {
             formdata[gf] = DATA[gf];
         }
     });
-    
+
     R_FIELDS.forEach(rf => {
         if (!DATE_FIELDS.includes(rf)) {
             formdata[rf] = s_data[rf];
         }
     })
-    
+
     function date_transform(old_value, new_postfix) {
         let oldv = new Date(old_value);
         if (oldv.getDate() != oldv.getDate()) {
@@ -258,32 +260,37 @@ function stampaj_saradnik(s_id) {
         }
         return oldv;
     }
-    
+
     date_transform(DATA.datump, 'trajanje');
     date_transform(DATA.datumk, 'trajanjedo');
 
     let dol = time_transform(s_data.dolazak, 'dolaska');
     let odl = time_transform(s_data.odlazak, 'odlaska');
-    
-    formdata.brojcasova = Math.floor((odl-dol)/(1000*60*60));
+
+    formdata.brojcasova = Math.floor((odl - dol) / (1000 * 60 * 60));
     formdata.dnevnica = DNEVNICA_MAP[formdata.status];
-    
+
+    if (s_data.status == "Rukovodioc") {
+        formdata.status = "Stručni"
+        formdata.poziv = "Jelena Gledić"
+    }
+
     formdata.akontacija = "";
     formdata.dana = new Date().getDate();
     formdata.meseca = new Date().getMonth() + 1;
     formdata.gode = new Date().getFullYear();
-    
+
     if (!stampaj_validate(formdata)) {
         return;
     }
-    
+
     const sumbitForm = document.getElementById('submit-form');
     sumbitForm.innerHTML = '';
-    
+
     for (const key in formdata) {
         if (Object.hasOwnProperty.call(formdata, key)) {
             const value = formdata[key];
-            
+
             const elem = document.createElement('input');
             elem.type = 'hidden';
             elem.value = value;
@@ -291,7 +298,7 @@ function stampaj_saradnik(s_id) {
             sumbitForm.appendChild(elem);
         }
     }
-    
+
     sumbitForm.submit();
 }
 
@@ -303,7 +310,7 @@ function brisi_saradnik(rm_id, rm_ime) {
     if (!confirm("Sigurno obrisati saradnika " + rm_ime + "?")) {
         return;
     }
-    
+
     DATA.saradnici = DATA.saradnici.filter(x => x.id != rm_id);
     render(true);
 }
@@ -334,27 +341,27 @@ function render(full) {
         vv.value = DATA[gf];
         vv.onchange = () => update();
     });
-    
+
     const tb = document.getElementById('table-body');
-    
+
     if (full) {
         tb.innerHTML = '';
     }
-    
+
     DATA.saradnici.forEach(sd => {
         const tr = document.createElement('tr');
         if (full) {
             tr.innerHTML = single_row(sd);
             tb.appendChild(tr);
         }
-        
+
         document.getElementById(`r-${sd.id}-stampaj`).onclick = () => stampaj_saradnik(sd.id);
         document.getElementById(`r-${sd.id}-brisi`).onclick = () => brisi_saradnik(sd.id, sd.imeprezime);
         document.getElementById(`r-${sd.id}-set-dolazak`).onclick = () => set_saradnik_start(sd.id);
         document.getElementById(`r-${sd.id}-set-odlazak`).onclick = () => set_saradnik_end(sd.id);
         document.getElementById(`r-${sd.id}-jpv-brisi`).onclick = () => brisi_jpv(sd.id);
         document.getElementById(`r-${sd.id}-spv-brisi`).onclick = () => brisi_spv(sd.id);
-        
+
         R_FIELDS.forEach(rf => {
             const ie = document.getElementById(`r-${sd.id}-${rf}`);
             ie.value = sd[rf];
@@ -376,4 +383,18 @@ function render(full) {
             };
         });
     });
+}
+
+function saradnik_export() {
+    let dataStr = JSON.stringify(DATA);
+
+    let elem = document.createElement('a');
+    elem.href = 'data:text/json;charset=utf-8,' + encodeURI(dataStr);
+    elem.target = "_blank";
+    elem.download = "putnigen-export.json";
+    elem.click();
+}
+
+function saradnik_import() {
+    alert('not implemented yet')
 }
